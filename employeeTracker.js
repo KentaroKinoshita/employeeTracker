c5f35a81ee1152c491dff1ae3c75d015e1244ff3
Kentaro Kinoshita <mixmix0321@gmail.com>
2020-10-28 15:04:14 -0400

    connected mysql to js

--- employeeTracker.js
+++ employeeTracker.js
@@ -1,3 +1,23 @@
 const mysql = require('mysql')
-const inquirer = reqipre('inqiurer')
+const inquirer = require('inquirer')
+
+// Connection Properties
+const connectionProperties = {
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Chie123.",
+    database: "employees_db"
+}
+
+// creating connection
+const connection = mysql.createConnection(connectionProperties);
+
+// Establishing connection to the database
+connection.connect((err) => {
+    if (err) throw err;
+
+    console.log("\n WELCOME TO EMPLOYEE TRACKER \n")
+    mainMenu();
+})
 
