fc03171faf890e3dc06b6840e6c046ec5fa6ff24
Kentaro Kinoshita <mixmix0321@gmail.com>
2020-10-28 15:11:05 -0400

    Created a function that ask actions

--- employeeTracker.js
+++ employeeTracker.js
@@ -1,5 +1,6 @@
 const mysql = require('mysql')
-const inquirer = require('inquirer')
+const inquirer = require('inquirer');
+const Choices = require('inquirer/lib/objects/choices');
 
 // Connection Properties
 const connectionProperties = {
@@ -21,3 +22,48 @@ connection.connect((err) => {
     mainMenu();
 })
 
+function mainMenu () {
+    // ask what they want to do
+    inquirer
+    .prompt({
+        name: "mainMenu",
+        type: "list",
+        message: "MAIN MENU",
+        choices: [
+            "View All Employees",
+            "View all employees by role",
+            "View all employees by department",
+            "View all employees by manager",
+            "Add employee",
+            "Add role",
+            "Add department"
+        ]
+    }).then((answer) => {
+        // switch actions
+        switch (answer.mainMenu) {
+            case "View all employees":
+                viewAllEmp();
+                break;
+
+            case "View all employees by department":
+                viewAllEmpByDept();
+                break;
+
+            case "View all employees by role":
+                viewAllEmpByRole();
+                break;
+
+            case "Add employee":
+                addEmp();
+                break;
+
+            case "Add department":
+                addDept();
+                break;
+
+            case "Add role":
+                addRole();
+                break;
+        }
+    })
+}
