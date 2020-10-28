6e6b38f27138f9fe6da0fa33cba111fbe3eef72d
Kentaro Kinoshita <mixmix0321@gmail.com>
2020-10-28 18:31:52 -0400

    Chaneged function

--- employeeTracker.js
+++ employeeTracker.js
@@ -69,10 +69,9 @@ function mainMenu () {
 
 function viewAllEmp(){
     // query to view all employees
-    let query = "SELECT e.id, e.first_name, e.last_name, role.title, department.name AS department, role.salary, concat(m.first_name, ' ' ,  m.last_name) AS manager FROM employee e LEFT JOIN employee m ON e.manager_id = m.id INNER JOIN role ON e.role_id = role.id INNER JOIN department ON role.department_id = department.id ORDER BY ID ASC";
-    
+    let quesry = "SELECT * FROM employee;"    
     // query from connection
-    connection.query(query, function (err, res) {
+    connection.query(query, function(err, res) {
         if (err) throw err;
         console.log("\n");
 
@@ -81,7 +80,7 @@ function viewAllEmp(){
 
         //Back to main menu
         mainMenu();
-    })
+    });
 }
 
 function viewAllEmpByDept(){
